98a608d572be04d819784e6ca6f6393a9fc1c1d7
gaojinshan83@gmail.com <Gaojinshan&83>
2022-07-26 17:17:34 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Vue 3 + Vite
+
+This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+
+## Recommended IDE Setup
+
+- [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar)
+
+## antdv UI 
+url:https://antdv.com/components/button-cn
+已设置按需引入
+
+## 云拷贝 计划引入这个编辑器
+https://microsoft.github.io/monaco-editor/
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <a-button type="primary">Primary Button</a-button>
+  <a-button>Default Button</a-button>
+  <a-button type="dashed">Dashed Button</a-button>
+  <a-button type="text">Text Button</a-button>
+  <a-button type="link">Link Button</a-button>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
