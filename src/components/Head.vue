4100872f60f3685249e098627364c04dddbcb9aa
gaojinshan83@gmail.com <Gaojinshan&83>
2022-07-28 15:51:02 +0800

    消息模块

--- src/components/Head.vue
+++ src/components/Head.vue
@@ -14,7 +14,21 @@
     <div class="boxes">
       <a-popover title="消息提醒" placement="topRight">
         <template #content>
-          <div class="1"></div>
+          <div class="messageWrap">
+            <div class="details">
+              <div class="text">
+                需要用户处理事务，又不希望跳转页面以致打断工作流程时，可以使用
+                Modal 在当前页面正中打开一个浮层，承载相应的操作。
+                另外当需要一个简洁的确认框询问用户时，可以使用 Modal.confirm()
+                等语法糖方法。
+              </div>
+              <span class="time">6个月前</span>
+            </div>
+            <div class="info">
+              <span>当前标记为已读</span>
+              <a-button type="text">查看全部</a-button>
+            </div>
+          </div>
         </template>
         <a-button type="primary" shape="circle" size="large">
           <template #icon><BellOutlined /></template>
@@ -124,7 +138,7 @@ const formatNumber = (value) => {
   const prefix = list[0].charAt(0) === "-" ? "-" : "";
   let num = prefix ? list[0].slice(1) : list[0];
   let result = "";
-  
+
   while (num.length > 3) {
     result = `,${num.slice(-3)}${result}`;
     num = num.slice(0, num.length - 3);
@@ -273,5 +287,25 @@ const LoggedOut = () => {
   display: flex;
   align-items: center;
   justify-content: center;
+}
+.messageWrap {
+  width: 400px;
+  .details {
+    padding: 0 0 1em;
+    border-bottom: 1px #f0f0f0 solid;
+    .text {
+    }
+    .time {
+      display: block;
+      margin-top: 10px;
+      color: #c6c6c6;
+    }
+  }
+  .info {
+    padding-top: 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 }
 </style>
