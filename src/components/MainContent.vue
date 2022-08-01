bf141cf2f369afbcc4384493de9046f322fc543b
gaojinshan83@gmail.com <Gaojinshan&83>
2022-08-01 15:24:42 +0800

    语言切换，有个神秘报错，待解决

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="codeEditWrap">
-    <div class="codeEditInfo"></div>
+    <div class="codeEditInfo">
+      <a-form layout="inline">
+        <a-form-item>
+          <a-select
+            v-model:value="value"
+            label-in-value
+            style="width: 150px"
+            :options="options"
+            @change="handleChange"
+          ></a-select>
+        </a-form-item>
+      </a-form>
+    </div>
     <div id="codeEditBox" class="codeEditBox"></div>
   </div>
 </template>
@@ -9,11 +21,15 @@ import * as monaco from "monaco-editor";
 
 import { computed, ref, watch, toRaw, onMounted } from "vue";
 const editor = ref(null);
+const value = ref({
+  value: "python",
+});
+const language = ref(value.value.value);
 const initEditor = () => {
   // 初始化编辑器，确保dom已经渲染
   editor.value = monaco.editor.create(document.getElementById("codeEditBox"), {
     value: "", //编辑器初始显示文字
-    language: "python", //此处使用的python，其他语言支持自行查阅demo
+    language: language.value, //此处使用的python，其他语言支持自行查阅demo
     theme: "vs-dark", //官方自带三种主题vs, hc-black, or vs-dark
     selectOnLineNumbers: true, //显示行号
     roundedSelection: false,
@@ -34,6 +50,34 @@ const initEditor = () => {
 onMounted(() => {
   initEditor();
 });
+
+const options = ref([
+  {
+    value: "python",
+    label: "python",
+  },
+  {
+    value: "javascript",
+    label: "javascript",
+  },
+  {
+    value: "html",
+    label: "html",
+  },
+  {
+    value: "css",
+    label: "css",
+  },
+]);
+
+const handleChange = (value) => {
+  console.log(value.key); // { key: "lucy", label: "Lucy (101)" }
+  language.value = value.key;
+  monaco.editor.setModelLanguage(
+    toRaw(editor.value).getModel(),
+    language.value
+  );
+};
 </script>
 <style lang="scss" scoped>
 .codeEditWrap {
