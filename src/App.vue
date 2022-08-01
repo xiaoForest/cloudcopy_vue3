3a742a3b57642df3f0f131ad7c4eb583d7cd2987
gaojinshan83@gmail.com <Gaojinshan&83>
2022-08-01 11:35:38 +0800

    第一步编辑器

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,34 @@
   <NavigationBar />
   <div class="containerFlex">
     <Menu />
-    <MainContent />
+    <MainContent
+      ref="monacoEdit"
+      :value="ruleForm.content"
+      @contentChange="contentChange"
+    />
   </div>
 </template>
 
 <script setup>
+import { computed, ref, watch, toRaw } from "vue";
+const countent = ref("");
+const props = defineProps({
+  propData: {
+    type: String,
+    default: "",
+  },
+});
+const ruleForm = ref({
+  content: "",
+});
+const getList = () => {
+  ruleForm.value.content = "console.log()";
+};
+function contentChange(val) {
+  //先拿一个值存放monaco组件传递过来的值
+  countent.value = val;
+  console.log(val);
+}
 </script>
 
 <style lang="scss" scoped>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import {
 } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite';
+
 import {
   AntDesignVueResolver
 } from 'unplugin-vue-components/resolvers';
@@ -11,6 +12,7 @@ export default defineConfig({
 
   plugins: [
     vue(),
+
     Components({
       resolvers: [AntDesignVueResolver()],
     }),
