97653f626756f7ef088edace8f3669e24c56755b
gaojinshan83@gmail.com <Gaojinshan&83>
2022-08-02 09:55:16 +0800

    清理app页面无用的代码

--- src/App.vue
+++ src/App.vue
@@ -4,34 +4,12 @@
   <NavigationBar />
   <div class="containerFlex">
     <Menu />
-    <MainContent
-      ref="monacoEdit"
-      :value="ruleForm.content"
-      @contentChange="contentChange"
-    />
+    <MainContent ref="monacoEdit" />
   </div>
 </template>
 
 <script setup>
 import { computed, ref, watch, toRaw } from "vue";
-const countent = ref("");
-const props = defineProps({
-  propData: {
-    type: String,
-    default: "",
-  },
-});
-const ruleForm = ref({
-  content: "",
-});
-const getList = () => {
-  ruleForm.value.content = "console.log()";
-};
-function contentChange(val) {
-  //先拿一个值存放monaco组件传递过来的值
-  countent.value = val;
-  console.log(val);
-}
 </script>
 
 <style lang="scss" scoped>
